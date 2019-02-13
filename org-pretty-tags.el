cdf5a48b21abe977944733ba4c1fab328e7f7102
Marco Wahl <marcowahlsoft@gmail.com>
2019-02-13 20:59:45 +0100

    el:~:new generation

--- org-pretty-tags.el
+++ org-pretty-tags.el
@@ -28,18 +28,18 @@
 
 ;;; Commentary:
 
+;; - Activate the mode with {M-x org-pretty-tags-mode RET}.
+;; - Deactivate the mode with a further {M-x org-pretty-tags-mode RET}.
+;;
 ;; Use {M-x customize-variable RET org-pretty-tags-surrogate-strings RET} to
 ;; define surrogate strings for tags.  E.g. add the pair "money", "$$$".
 ;; 
 ;; If you don't like the predefined surrogates then just delete them.
 ;; 
-;; Use {M-x customize-variable RET org-pretty-tags-surrogate-images} to
+;; Use {M-x customize-variable RET org-pretty-tags-surrogate-images RET} to
 ;; define surrogate images for tags.  The definition of the image is
 ;; expected to be a path to an image.  E.g. add the pair "org", "<path to
 ;; org icon>".
-;;
-;; - Activate the mode with {M-x org-pretty-tags-mode RET}.
-;; - Deactivate the mode with a further {M-x org-pretty-tags-mode RET}.
 ;;
 ;; See also the literate source file.  E.g. see https://gitlab.com/marcowahl/org-pretty-tags.
 
