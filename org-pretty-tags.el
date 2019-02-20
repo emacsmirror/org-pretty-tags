d8cd5282d334829468d940664d92853f1fb51bf4
Marco Wahl <marcowahlsoft@gmail.com>
2019-02-20 21:13:10 +0100

    ~:version update

--- org-pretty-tags.el
+++ org-pretty-tags.el
@@ -7,7 +7,7 @@
 ;; Author: Marco Wahl <marcowahlsoft@gmail.com>
 ;; Maintainer: Marco Wahl <marcowahlsoft@gmail.com>
 ;; Created: [2019-01-06]
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "25"))
 ;; Keywords: reading, outlines
 ;; URL: https://gitlab.com/marcowahl/org-pretty-tags
